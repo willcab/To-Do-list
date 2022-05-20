c8de8a0454dbe5d324be122bb94bb93e6442262c
willcab <willianeduardocg13@gmail.com>
2022-05-20 11:44:11 -0500

    edit Lane, show tickets

--- src/components/Lane/Lane.js
+++ src/components/Lane/Lane.js
@@ -1,28 +1,41 @@
 import React from "react";
-import styled from 'styled-components';
+import styled from "styled-components";
+import Ticket from "../Ticket/Ticket";
 
 const LaneWrapper = styled.div`
-  list-style: none;
-  text-align: left;
-  padding: 0;
-  background: lightGray;
-  border-radius: 20px;
-  min-height: 50vh;
-  width: 20vw;
-  @media (max-width: 768px) {
-    margin-bottom: 5%;
-  }
+	list-style: none;
+	text-align: left;
+	padding: 0;
+	background: lightGray;
+	border-radius: 20px;
+	min-height: 50vh;
+	width: 20vw;
+	@media (max-width: 768px) {
+		margin-bottom: 5%;
+	}
 `;
 
 const Title = styled.h2`
-  width: 100%;
-  padding-bottom: 10px;
-  text-align: center;
-  border-bottom: 1px solid darkGray;
+	width: 100%;
+	padding-bottom: 10px;
+	text-align: center;
+	border-bottom: 1px solid darkGray;
 `;
-const Lane = ({title}) => (
-    <LaneWrapper>
-        <Title>{title}</Title>
-    </LaneWrapper>
+const TicketsWrapper = styled.div`
+	padding: 5%;
+`;
+const Alert = styled.div`
+	text-aling: center;
+`;
+const Lane = ({ tickets, loading, error, title }) => (
+	<LaneWrapper>
+		<Title>{title}</Title>
+		{(loading || error) && <Alert>{loading ? "Loading..." : error}</Alert>}
+		<TicketsWrapper>
+			{tickets.map((ticket) => (
+				<Ticket key={ticket.id} ticket={ticket} />
+			))}
+		</TicketsWrapper>
+	</LaneWrapper>
 );
 export default Lane;
