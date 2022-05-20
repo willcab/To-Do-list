37ff5b96baf651f422cd45c36e1158b99b795682
willcab <willianeduardocg13@gmail.com>
2022-05-20 11:30:42 -0500

    get data, board

--- src/containers/Board.js
+++ src/containers/Board.js
@@ -1,34 +1,66 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import Lane from '../components/Lane/Lane';
+import React, { Component } from "react";
+import styled from "styled-components";
+import Lane from "../components/Lane/Lane";
 
 const BoardWrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-  flex-direction: row;
-  margin: 5%;
-  @media (max-width: 768px) {
-    flex-direction: column;
-  }
+	display: flex;
+	justify-content: space-between;
+	flex-direction: row;
+	margin: 5%;
+	@media (max-width: 768px) {
+		flex-direction: column;
+	}
 `;
 
-class Board extends Component{
-    render(){
-        const lanes=[
-            {id: 1, title: 'To Do' },
-            { id: 2, title: 'In Progress' },
-            { id: 3, title: 'Review' },
-            { id: 4, title: 'Done' },
-        ];
-        return(
-            <BoardWrapper>
-                {
-                    lanes.map(lane=>(
-                        <Lane key={lane.id} title={lane.title}/>
-                    ))
-                }
-            </BoardWrapper>
-        );
-    }
+class Board extends Component {
+	constructor() {
+		super();
+		this.state = {
+			data: [],
+			loading: true,
+			error: "",
+		};
+	}
+	async componentDidMount() {
+		try {
+			const tickets = await fetch("../../assets/data.json");
+			const ticketsJSON = await tickets.json();
+
+			if (ticketsJSON) {
+				this.setState({
+					data: ticketsJSON,
+					loading: false,
+				});
+			}
+		} catch (error) {
+			this.setState({
+				loading: false,
+				error: error.message,
+			});
+		}
+	}
+
+	render() {
+		const { data, loading, error } = this.state;
+		const lanes = [
+			{ id: 1, title: "To Do" },
+			{ id: 2, title: "In Progress" },
+			{ id: 3, title: "Review" },
+			{ id: 4, title: "Done" },
+		];
+		return (
+			<BoardWrapper>
+				{lanes.map((lane) => (
+					<Lane
+						key={lane.id}
+						title={lane.title}
+						loading={loading}
+						error={error}
+						tickets={data.filter((ticket) => ticket.lane === lane.id)}
+					/>
+				))}
+			</BoardWrapper>
+		);
+	}
 }
 export default Board;
